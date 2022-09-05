dbb6f9cdd39efc433e651035f9c010d87a843354
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-09-05 14:29:28 +0800

    Modified blade layout.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="/app.css">
-   <script src="/app.js"></script>
-   <title>My Blog Posts</title>
-</head>
-
-<body>
-   <?php foreach ($posts as $post) : ?>
-      <article>
-         <h1>
-            <a href="/posts/<?= $post->slug; ?>">
-               <?= $post->title; ?>
-            </a>
-         </h1>
-         <p><?= $post->excerpt; ?></p>
-      </article>
-   <?php endforeach; ?>
-</body>
-
-</html>
+<x-layout>
+   @foreach ($posts as $post)
+   <article>
+      <h1>
+         <a href="/posts/{{ $post->slug }}">
+            {{ $post->title }}
+         </a>
+      </h1>
+      <p>{{ $post->excerpt }}</p>
+   </article>
+   @endforeach
+</x-layout>
